ee96ed1e9ed4a34e1f225026bc3245d429c22550
Kelsey Breseman <ifoundthemeaningoflife@gmail.com>
2016-07-14 11:10:54 +0100

    initial functioning tweeting tesselcam

---
+++ README.md
@@ -0,0 +1,15 @@
+Photo Tweeter
+=============
+Tweets photos.
+
+## Materials
+* Tessel 2
+* Webcam
+
+## To use
+1. Plug in USB webcam to Tessel 2
+1. Clone this repo and `npm install`
+1. Make sure Tessel is connected to Wifi (`t2 wifi`)
+1. `t2 run index.js` to run
+
+Ta-da! Check @TesselTweet's twitter for your tweeted photo.
---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var path = require('path');
+var filepath = 'myImage.jpg';
+
+// Set up the camera
+var av = require('tessel-av');
+var camera = new av.Camera();
+
+// Set up Twitter
+var twitter = require('twit');
+// These are the public "secret" keys of the TesselTweet account. They are public online.
+var twit = new twitter({
+  consumer_key: 'O7oc0pvsZn4xjgcuHuYdX4FaC',
+  consumer_secret: 'iJYuHFz2sD46Nvk3mcwzX8uih14aEAMgVWdWoR59nx8v6Zl7ZX',
+  access_token: '2529232909-luARGU89K4CKFMvfzBjCgG6ubefzDkdDWkSB85i',
+  access_token_secret: 'GXQfuzvGdjLEs3t1HEYfhQ9x9bdBcSBVXjBkbRgwYlOE0'
+});
+
+// Take a picture
+var capture = camera.capture();
+capture.on('data', function(data) {
+  // Make post request on media endpoint. Pass file data as media parameter
+  twit.post('media/upload', {media_data: data.toString('base64')}, function(error, media, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      // If successful, a media object will be returned.
+      console.log(media);
+
+      // Let's tweet it
+      var status = {
+        status: 'I am a tweet',
+        media_ids: media.media_id_string // Pass the media id string
+      };
+
+      twit.post('statuses/update', status, function(error, tweet, response) {
+        if (!error) {
+          console.log(tweet);
+        }
+      });
+    }
+  });
+});
